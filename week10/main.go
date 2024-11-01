6edc6b6bb50e75ef1e0d852d6df2a150d2698a5e
ygy0518 <ygy0518@gmail.com>
2024-11-01 14:26:57 +0900

    v2.4 prime number

--- week10/main.go
+++ week10/main.go
@@ -23,19 +23,20 @@ func main() {
 		log.Fatal(err)
 	}
 
-	//counts := 0
-	var isPrime bool = true // 가독성 up, 메모리 down
-	j := 2
-	for j < n {
-		if n%j == 0 {
-			//counts = counts + 1
-			isPrime = false // 더하기 연산 제거
+	var isPrime bool = true
+	// bug fix
+	if n <= 1 {
+		isPrime = false
+	} else {
+		j := 2
+		for j < n {
+			if n%j == 0 {
+				isPrime = false
+			}
+			j++
 		}
-		j++
 	}
-
-	//if counts == 0 {
-	if isPrime { // ==비교 연산 제거
+	if isPrime {
 		fmt.Printf("%d is prime number", n)
 	} else {
 		fmt.Printf("%d is not prime number", n)
